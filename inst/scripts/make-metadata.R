d0d4eccfbc7b66ecf3df5d772200713e68e5769b
StefanGraw <shgraw@uams.edu>
2019-08-07 10:43:17 -0500

    Addressing issues with meta data and package

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -1,5 +1,6 @@
+# setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 df = data.frame("Title" = NA, 
-                "Decription" = NA, 
+                "Description" = NA, 
                 "BiocVersion" = NA, 
                 "Genome" = NA, 
                 "SourceType" = NA, 
@@ -13,13 +14,12 @@ df = data.frame("Title" = NA,
                 "RDataClass" = NA, 
                 "DispatchClass" = NA, 
                 "RDataPath" = NA, 
-                "Tags" = NA, 
                 "Notes" = NA)
 
 
 df[1:12,] = matrix(c("Saliva", "Lymphoma", "Placenta", "Liver", "Colon", "Blood (Adults) - Peripheral Leukocytes", "Blood 5 year olds", "Blood newborns", "Cord-blood (whole blood)", "Cord-blood (PBMC)", "Adult (PBMC)", "Sperm",
                      paste("Genome-wide CpG-specific means and variance for ", c("Saliva", "Lymphoma", "Placenta", "Liver", "Colon", "Blood (Adults) - Peripheral Leukocytes", "Blood 5 year olds", "Blood newborns", "Cord-blood (whole blood)", "Cord-blood (PBMC)", "Adult (PBMC)", "Sperm"), sep = ""),
-                     rep("3.9", 12),
+                     rep("3.10", 12),
                      rep(NA,12), # rep("HM450k", 12),
                      rep("TXT",12),
                      paste("https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=", c("GSE92767", "GSE42372", "GSE62733", "GSE61258", "GSE77718", "GSE42861", "GSE83334", "GSE82273", "GSE69176", "GSE110128", "GSE67170", "GSE114753"), sep = ""),
@@ -28,13 +28,12 @@ df[1:12,] = matrix(c("Saliva", "Lymphoma", "Placenta", "Liver", "Colon", "Blood 
                      rep(9606,12),
                      rep(NA,12),
                      rep("GEO",12),
-                     rep("sgraw@kumc.edu",12),
-                     rep(NA,12),
+                     rep("shgraw@uams.edu",12),
+                     rep("data.frame",12),
                      rep("Rda",12),
-                     rep("path?",12),
-                     rep(NA,12),
+                     paste("pwrEWAS_data/", c("Saliva.rdata", "Lymphoma.rdata", "Placenta.rdata", "Liver.rdata", "Colon.rdata", "Blood_adult.rdata", "Blood_5yrOlds.rdata", "BloodNewborns.rdata", "CordBlood_wholeBlood.rdata", "CordBlood_PBMC.rdata", "Adult_PBMC.rdata", "Sperm.rdata"), sep = ""),
                      rep("HM450k",12)),
-                   nrow = 12, ncol = 17, byrow = F
+                   nrow = 12, ncol = 16, byrow = F
 )
 
 write.csv(df, file = "../extdata/metadata.csv", row.names=FALSE)
